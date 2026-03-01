b2afbf74a961ccd8f729dc74345ca705a2fab3e9
EthanHunt2212 <ethan.hunt@students.plymouth.ac.uk>
2026-03-01 20:22:54 +0000

    WEB SERVER ETHAN

---
+++ SourceCode/server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        const filePath = path.join(__dirname, 'buttons.html');
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end('Error loading the page');
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                res.end(data);
+            }
+        });
+    } else {
+        // simple 404 for other routes
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Not found');
+    }
+});
+
+const PORT = 6767;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
